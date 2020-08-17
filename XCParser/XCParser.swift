1852e265cb2e0ccad785e59f1e4d9a9adf469dec
Soaurabh Kakkar <soaurabh.kakkar@gmail.com>
2020-08-18 01:46:14 +0530

    Generalise testsRefIds and summaryRefIds to account for all nested objects

--- XCParser/XCParser.swift
+++ XCParser/XCParser.swift
@@ -11,24 +11,27 @@ struct XCParser {
     let xcresultBundle: String
     
     func testSummaries() -> [ActionTestSummary] {
-        guard let testsRefId = self.testsRefId() else { return [] }
+        let testsRefIds = self.testsRefIds()
+        guard !testsRefIds.isEmpty else { return [] }
         
-        let summaryRefIds = self.summaryRefIds(from: testsRefId)
+        let summaryRefIds = testsRefIds.reduce([String]()) { (accumulator, testsRefId) -> [String] in
+            return accumulator + self.summaryRefIds(from: testsRefId)
+        }
         guard !summaryRefIds.isEmpty else { return [] }
         
         return self.testSummaries(from: summaryRefIds)
     }
 
-    private func testsRefId() -> String? {
+    private func testsRefIds() -> [String] {
         let arguments = Constants.xcresulttoolArg + [xcresultBundle]
         
         let executor = Executor(command: Command(launchPath: Constants.xcrunExecPath, arguments: arguments))
         let record = executor.exec(for: ActionsInvocationRecord.self)
         
-        guard let invocationRecord = record else { return nil }
+        guard let invocationRecord = record else { return [] }
         
-        let testRefId = invocationRecord.actions.first?.actionResult.testsRef.id
-        return testRefId
+        let testRefIds = invocationRecord.actions.map{ $0.actionResult.testsRef.id }
+        return testRefIds
     }
 
     private func summaryRefIds(from testsRefId: String) -> [String] {
@@ -40,16 +43,27 @@ struct XCParser {
         
         guard let testPlanRunSummaries = runSummaries else { return summaryRefIds }
         
-        let testableSummaries = testPlanRunSummaries.summaries.first?.testableSummaries
-        guard let testableSummaryValues = testableSummaries else { return summaryRefIds }
+        let testableSummaries = testPlanRunSummaries.summaries.flatMap{ $0.testableSummaries }
 
-        summaryRefIds = testableSummaryValues.reduce([String]()) { (accumulator, testableSummaryValue) -> [String] in
-            let summaryIdentifiableObject =  testableSummaryValue.tests.first?.subtests?.first?.subtests?.first?.subtests
-            guard let testSummaryIdentifiableObject = summaryIdentifiableObject else { return accumulator }
-            let partialRefIds = testSummaryIdentifiableObject.compactMap({ $0.summaryRef?.id })
+        summaryRefIds = testableSummaries.reduce([String]()) { (accumulator, testableSummary) -> [String] in
+            let testSummaryIdentifiableObjects = self.testSummaryIdentifiableObjects(for: testableSummary.tests)
+            let partialRefIds = testSummaryIdentifiableObjects.compactMap({ $0.summaryRef?.id })
             return accumulator + partialRefIds
         }
         return summaryRefIds
+    }
+
+    private func testSummaryIdentifiableObjects(for identifiableObjects: [ActionTestSummaryIdentifiableObject]) -> [ActionTestSummaryIdentifiableObject] {
+        var summaryIdentifiableObjects = [ActionTestSummaryIdentifiableObject]()
+    
+        for summaryIdentifiableObject in identifiableObjects {
+            if let IdentifiableObjectSubtests = summaryIdentifiableObject.subtests {
+                summaryIdentifiableObjects += self.testSummaryIdentifiableObjects(for: IdentifiableObjectSubtests)
+            } else {
+                summaryIdentifiableObjects.append(summaryIdentifiableObject)
+            }
+        }
+        return summaryIdentifiableObjects
     }
 
     private func testSummaries(from summaryRefIds: [String]) -> [ActionTestSummary] {
