a60ec702bc7ac1d69472a3f0d792cad38fe17e7c
Soaurabh Kakkar <soaurabh.kakkar@gmail.com>
2020-10-07 17:43:13 +0530

    Change xcodebuildExecPath from Xcode-beta to Xcode stable

--- XCTool/Constants.swift
+++ XCTool/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum Constants {
     static let xcrunExecPath = "/usr/bin/xcrun"
-    static let xcodebuildExecPath = "/Applications/Xcode-beta.app/Contents/Developer/usr/bin/xcodebuild"
+    static let xcodebuildExecPath = "/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild"
     static let xcresulttoolArg = ["xcresulttool", "get", "--format", "json", "--path"]
     static let xcresultIdArg = "--id"
     static let maxRetries = 3
